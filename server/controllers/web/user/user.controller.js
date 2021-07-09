55a39f74eca604a9e6d11d6d155665a61e1677ac
YasiruAyesh97 <yashiruayashmantha@gmail.com>
2021-07-09 12:25:48 +0530

    user token added to the database

--- server/controllers/web/user/user.controller.js
+++ server/controllers/web/user/user.controller.js
@@ -1,7 +1,7 @@
-const {sequelize,webuser,webuserrole}=require('../../../models');
+
+const {webuser,webuserrole,reset_password}=require('../../../models');
 const userValidation=require('../../../validation/web/user/user.validation');
-const bcrypt = require('bcrypt');
-const jwt = require("jsonwebtoken");
+const crypto  = require('crypto');
 
 exports.registerController = async (req, res) => {
     try{
@@ -26,9 +26,28 @@ exports.registerController = async (req, res) => {
             service_id,
         };
 
-         await webuser.create(user);
-        return res.status(200).send("user successfully registerd");
+        const successUser = await webuser.create(user);
+        if(!successUser) return res.status(404).send("User not registered");
+
+        const token = crypto.randomBytes(64).toString('base64');
+
+        let expireDate = new Date();
+        expireDate.setDate(expireDate.getDate() + 1);
+
+        const tokenData={
+            user_id:successUser.id,
+            email:email,
+            auth_token:token,
+            used: 1,
+            expire:expireDate
+        }
+        const successToken = await reset_password.create(tokenData);
+        if(!successToken) return res.status(404).send("token recode not added to the database!");
+
+
+        return res.status(200).send("User was registered successfully!");
     }catch (e) {
         return res.status(500).send("Internal Server Error");
     }
 }
+
