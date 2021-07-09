acec58e65f358cc5cafde2dc9fbb2c6cb33dc004
YasiruAyesh97 <yashiruayashmantha@gmail.com>
2021-07-09 20:23:22 +0530

    build backend  route for get the users data and create associate with webuser and user role table

--- server/router/web/user/user.route.js
+++ server/router/web/user/user.route.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const {registerController} =require('../../../controllers/web/user/user.controller');
-
+const {ViewController} =require('../../../controllers/web/user/userView.controller');
 
 router.post('/register',registerController);
-
+router.post('/view',ViewController);
 
 module.exports =router;
