44494756397b25ed35898b0ac818158b3d55f81c
YasiruAyesh97 <yashiruayashmantha@gmail.com>
2021-07-09 23:29:55 +0530

    edit user backend functionalites created

---
+++ server/controllers/web/user/userEdit.controller.js
@@ -0,0 +1,30 @@
+const {sequelize,webuser,webuserrole}=require('../../../models');
+const userValidation=require('../../../validation/web/user/userEdit.validation');
+
+
+exports.EditController = async (req, res) => {
+    try{
+
+        const {error} = userValidation(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        const {first_name,last_name, email,role_id } = req.body;
+
+        const foundUser = await webuser.findOne({ where: { id:req.params.id } });
+        if (!foundUser) return res.status(400).send("not user fot given id");
+
+        const foundRole = await webuser.findOne({ where: { id:role_id } });
+        if (!foundRole) return res.status(400).send("not user fot given id");
+
+
+        webuser.update(
+            { first_name: first_name, last_name: last_name,email:email,role_id:role_id },
+            { where: { id: req.params.id } }
+        );
+
+        return res.status(200).send("user successfully updated!");
+    }catch (e) {
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
--- server/router/web/user/user.route.js
+++ server/router/web/user/user.route.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const {registerController} =require('../../../controllers/web/user/user.controller');
 const {ViewController} =require('../../../controllers/web/user/userView.controller');
-
+const {EditController} =require('../../../controllers/web/user/userEdit.controller');
 router.post('/register',registerController);
 router.post('/view',ViewController);
+router.put('/update/:id',EditController);
 
 module.exports =router;
