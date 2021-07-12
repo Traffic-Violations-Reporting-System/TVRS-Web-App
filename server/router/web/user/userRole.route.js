b7cb12189b26c42139dfc9c2bd732513f3f1aa0e
uvinidesilva22 <2018is013@stu.ucsc.cmb.ac.lk>
2021-07-12 23:56:02 +0530

    create backend functions for insert user role

--- server/router/web/user/userRole.route.js
+++ server/router/web/user/userRole.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {userRoleController} =require('../../../controllers/web/user/userRole.controller');
+const {userRoleController,userRoleAddController} =require('../../../controllers/web/user/userRole.controller');
 
 
 router.get('/roles',userRoleController);
-
+router.post('/rolesadd',userRoleAddController);
 
 module.exports =router;
