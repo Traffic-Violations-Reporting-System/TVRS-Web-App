e22c921c8a4732fb2a8ec5b68f50922d054c2f82
thisaraMJ <madushantjg@gmail.com>
2021-07-14 01:38:11 +0530

    level3 dashboard view update 1.0

--- client/src/views/dashboard/DashboardLevel3.js
+++ client/src/views/dashboard/DashboardLevel3.js
@@ -1,12 +1,113 @@
-import React from 'react'
+import React, { lazy } from 'react'
+import {
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCol,
+  CProgress,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+
+import MainChartExample from '../charts/MainChartExample'
+const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
+
 
 
 const Dashboard = () => {
   return (
     <>
-      <h1>WELLCOME TO Level 3 DASHBORD</h1>
+      <WidgetsDropdown />
+      <CCard>
+        <CCardBody>
+          <CRow>
+            <CCol sm="5">
+              <h4 id="traffic" className="card-title mb-0">Accidents</h4>
+              <div className="small text-muted">November 2020</div>
+            </CCol>
+            <CCol sm="7" className="d-none d-md-block">
+              <CButton color="primary" className="float-right">
+                <CIcon name="cil-cloud-download"/>
+              </CButton>
+              <CButtonGroup className="float-right mr-3">
+                {
+                  ['Day', 'Month', 'Year'].map(value => (
+                    <CButton
+                      color="outline-secondary"
+                      key={value}
+                      className="mx-0"
+                      active={value === 'Month'}
+                    >
+                      {value}
+                    </CButton>
+                  ))
+                }
+              </CButtonGroup>
+            </CCol>
+          </CRow>
+          <MainChartExample style={{height: '300px', marginTop: '40px'}}/>
+        </CCardBody>
+        <CCardFooter>
+          <CRow className="text-center">
+            <CCol md sm="12" className="mb-sm-2 mb-0">
+              <div className="text-muted">Car Accidents</div>
+              <strong>29 Drivers (40%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="success"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
+              <div className="text-muted">Bike Accidents</div>
+              <strong>24 Drivers (20%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="info"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0">
+              <div className="text-muted">Bus Accidents</div>
+              <strong>78 Drivers (60%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="warning"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0">
+              <div className="text-muted">Three Wheeler Accidents</div>
+              <strong>22 Drivers (30%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="danger"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
+              <div className="text-muted">Other Accidents</div>
+              <strong>37 Drivers (40.15%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                value={40}
+              />
+            </CCol>
+          </CRow>
+        </CCardFooter>
+      </CCard>
     </>
   )
 }
 
 export default Dashboard
+
+
