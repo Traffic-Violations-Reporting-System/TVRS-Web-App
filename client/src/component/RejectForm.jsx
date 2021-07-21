fe1d8beddb376116cd63dd95f32eda69c1b6afb8
YasiruAyesh97 <yashiruayashmantha@gmail.com>
2021-07-21 23:27:59 +0530

    create basic Reject Details Update Form

--- client/src/component/RejectForm.jsx
+++ client/src/component/RejectForm.jsx
@@ -1,11 +1,94 @@
 import React from 'react';
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CFormGroup,
+  CInput,
+  CLabel,
+  CRow,
+  CSelect,
+  CTextarea
+} from "@coreui/react";
+import DocsLink from "../reusable/DocsLink";
 
 const RejectForm = () => {
 
   return (
     <div>
-      <h3>Reject Form</h3>
+      <CCard>
+        <CCardHeader>
+          <h5>Reject Details Update Form</h5>
+        </CCardHeader>
+        <CCardBody>
+          <CRow>
+            <CCol xs="12">
+              <CFormGroup>
+                <CLabel htmlFor="name">Description</CLabel>
+                <CTextarea
+                  name="textarea-input"
+                  id="textarea-input"
+                  rows="6"
+                  placeholder="Content..."
+                />
+              </CFormGroup>
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol xs="12">
+              <CFormGroup>
+                <CLabel htmlFor="ccyear">User Rating</CLabel>
+                <CSelect custom name="ccyear" id="ccyear">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </CSelect>
+              </CFormGroup>
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol xs="12">
+              <CFormGroup>
+                <CLabel htmlFor="ccmonth">Reason</CLabel>
+                <CSelect custom name="ccmonth" id="ccmonth">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
 
+                </CSelect>
+              </CFormGroup>
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol xs="4">
+              <CButton
+                color="primary"
+                className="w-md waves-effect waves-light"
+                style={{width:"35%"}}
+                type="submit"
+              >
+                Submit
+              </CButton>
+            </CCol>
+          </CRow>
+        </CCardBody>
+      </CCard>
 
 
     </div>
