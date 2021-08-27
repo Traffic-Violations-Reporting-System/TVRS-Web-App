152033463dd54d0a3e8eb953c6639d92dae95828
thisaraMJ <madushantjg@gmail.com>
2021-08-28 00:57:39 +0530

    setup user service for level3

--- client/src/services/web/level3UserService.js
+++ client/src/services/web/level3UserService.js
@@ -3,8 +3,19 @@ import jwtDecode from "jwt-decode";
 const config = require("../../config.json");
 
 export async function getNewComplaints() {
-   return await http.get(`${config["BASEURL"]}`+'/'+'web/user/view');
+  return await http.get(`${config["BASEURL"]}` + '/web/level3/getnew');
+  //this will returns complaints those are status==accepted
 }
-export function getComplain(id){
-  return  http.get(`${config["BASEURL"]}`+'/'+'web/complaine/get'+'/'+id);
+
+export async function getComplaints() {
+  return await http.get(`${config["BASEURL"]}` + '/web/level3/getall');
+  //this will returns complaints those are status==ongoing or status==completed
+}
+
+export function getFullComplaint(id){
+  return  http.get(`${config["BASEURL"]}`+'/web/level3/get/'+id);
+}
+
+export function updateComplaint(id){
+  return  http.post(`${config["BASEURL"]}`+'/web/level3/update/'+id);
 }
