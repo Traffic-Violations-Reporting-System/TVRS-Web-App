a0c8a2ff515a1317459cfe7f192057b473085e4d
thisaraMJ <madushantjg@gmail.com>
2021-09-13 10:54:53 +0530

    update level3UserServices

--- client/src/services/web/level3UserService.js
+++ client/src/services/web/level3UserService.js
@@ -2,20 +2,19 @@ import http from '../httpService';
 const config = require("../../config.json");
 
 export async function getNewComplaints(region) {  
-  return await http.get(`${config["BASEURL"]}` + '/web/level3/getnew/'+region);
+  return await http.get(`${config["BASEURL"]}/web/level3/getnew/`+region);
   //this will returns complaints those are status==Accepted
 }
 
 export async function getComplaints(region) {
-  return await http.get(`${config["BASEURL"]}` + '/web/level3/getall/'+region);
+  return await http.get(`${config["BASEURL"]}/web/level3/getall/`+region);
   //this will returns complaints those are status==Ongoing or status==Completed
 }
 
 export function getFullComplaint(id){
-  return  http.get(`${config["BASEURL"]}`+'/web/level3/get/'+id);
+  return  http.get(`${config["BASEURL"]}/web/level3/get/`+id);
 }
 
-export function updateComplaint(id, complaint) {
-  console.log(complaint);
-  // return  http.put(`${config["BASEURL"]}`+'/web/level3/update/'+id+'/'+complaint);
+export function updateComplaint(complaint) {
+  return  http.put(`${config["BASEURL"]}/web/level3/update`,complaint);
 } 
